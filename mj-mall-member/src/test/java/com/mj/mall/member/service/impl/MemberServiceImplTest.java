3d9b7f7830a7ca206910034809cf23a816d1386b
shelimingming <shelimingming@qq.com>
2019-04-14 22:42:06 +0800

    增加Sl4J日志功能

--- mj-mall-member/src/test/java/com/mj/mall/member/service/impl/MemberServiceImplTest.java
+++ mj-mall-member/src/test/java/com/mj/mall/member/service/impl/MemberServiceImplTest.java
@@ -3,14 +3,19 @@ package com.mj.mall.member.service.impl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import test.MJMallTest;
 
 import static org.junit.Assert.*;
 
 public class MemberServiceImplTest extends MJMallTest {
 
+    final static Logger logger = LoggerFactory.getLogger(Test.class);
+
     @Before
     public void setUp() throws Exception {
+        logger.info("测试：{}", "输出日志");
     }
 
     @After
