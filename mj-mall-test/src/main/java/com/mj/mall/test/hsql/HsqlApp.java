36e713ee6cd6babc42f413835a1bd58ecf5bdb76
shelimingming <shelimingming@qq.com>
2019-04-24 00:19:33 +0800

    test

--- mj-mall-test/src/main/java/com/mj/mall/test/hsql/HsqlApp.java
+++ mj-mall-test/src/main/java/com/mj/mall/test/hsql/HsqlApp.java
@@ -1,9 +1,13 @@
 package com.mj.mall.test.hsql;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.mj.mall.test.hsql")
+@MapperScan("com.mj.mall.test.hsql")
 public class HsqlApp {
 
     public static void main(String[] args) {
